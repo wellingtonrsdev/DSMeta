2c18533e7954cbc60d919d42f4ff2db5c1550140
Wellington Rodrigues <rodrigueswellington3@gmail.com>
2022-07-16 15:16:50 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -18,12 +18,18 @@ function SalesCard() {
   const [sales, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales`)
+
+    const dmin = minDate.toISOString().slice(0, 10);
+    const dmax = maxDate.toISOString().slice(0, 10);
+
+    console.log(dmin);
+
+    axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
       .then(Response => {
         setSales(Response.data.content);
 
       })
-  }, []);
+  }, [minDate, maxDate]);
 
   return (
     <div className="dsmeta-card">
